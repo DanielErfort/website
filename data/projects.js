1e6142182fc7f0cbd9401d0146a6566f7e08cb58
Daniel Erfort <daniel.erfort@gmail.com>
2023-06-03 23:46:44 +0200

    flip card added

--- data/projects.js
+++ data/projects.js
@@ -1,7 +1,10 @@
 const projects =  [{
   title: 'FOODBLOG',
   image: 'images/foodblog.jpg',
-  description: 'Hallo Welt <br> Das ist ein Test'
+  description: `
+    <p class="project-text">Hallo Welt</p>
+    <p class="project-text">Hallo Welt nochmla</p>
+  `
 }, {
   title: 'FOODBLOG',
   image: 'images/foodblog.jpg',
--- scripts/projects.js
+++ scripts/projects.js
@@ -12,19 +12,33 @@ let projectsHTML = '';
 projects.forEach((project) => {
   projectsHTML += 
   `
-  <div class="project-container">
-    <div class="project-preview ">
-      <img class="project-image " src="${project.image}">
-      <p class="project-title">${project.title}</p>
-    </div>
-    <div class="project-description">
-      <p>
-        ${project.description}
-      </p>
+  <div class="poject-container transition-element-bottom">
+    <div class="project-card">
+      <div class="project-front">
+        <img class="project-image" src="${project.image}" alt="Foodblog" loading="lazy">
+        <p class="project-title">${project.title}</p>
+      </div>
+      
+      <div class="project-back">
+        <p class="project-text">${project.description}</p>
+        <button class="button-project">Take a look!</button>
+      </div>
     </div>
   </div>
   `
 });
+
+{/* <div class="project-container">
+<div class="project-preview ">
+  <img class="project-image " src="${project.image}">
+  <p class="project-title">${project.title}</p>
+</div>
+<div class="project-description">
+  <p>
+    ${project.description}
+  </p>
+</div>
+</div>  */}
 
 projectsHTML += '<div class="projects-bottom-margin"></div>';
 
