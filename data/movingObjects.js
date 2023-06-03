9ef45bd892ed5d5c83ce7fa218bedbcbb0da05e1
Daniel Erfort <daniel.erfort@gmail.com>
2023-06-03 19:44:48 +0200

    Überarbeitet

--- data/movingObjects.js
+++ data/movingObjects.js
@@ -1,20 +1,26 @@
-const objectsCount = 35;
+const objectsCount = 30;
 let movingObjects = [];
 
 for(let i = 1; i <= objectsCount; i++){
   const width = Math.random() * 80 + 20;
-  const height = Math.random() * 80 + 20;
+  //const height = Math.random() * 80 + 20;
+  const height = width;
   const positionTop = Math.random() * 92;
   const opacity = Math.random() * 67; //max. opacity 67%
-  const color = (Math.random()<0.5)? 'rgb(255, 64, 129)' : 'rgb(0, 188, 212)';
+  //const color = (Math.random()<0.5)? 'rgb(255, 64, 129)' : 'rgb(0, 188, 212)';
+  const color = (Math.random()<0.5)? 'white' : 'rgb(0, 188, 212)';
   const duration = Math.random() * 40 + 20; // Zufallszahl zwischen 20 und 60
 
   const translation = (Math.random() * 45 + 5);
-  const translationStart = (Math.random()<0.5)? translation+100 : translation*-1;
-  const translationEnd = translationStart * -1 + 100;
+  //const translationStart = (Math.random()<0.5)? translation+100 : translation*-1;
+  const translationStart = (Math.random()<0.5)? 100 : -10;
+  const translationEnd = translationStart * -1 + 90;
 
   const rotationStart = Math.random() * 45 * 40; // erste Zahl: 45°; zweite Zahl: Multiplikator (max 50)
   const rotationEnd = rotationStart * 2;
+
+  //const delay = Math.random() * 10;
+  const delay = i;
 
   movingObjects.push({
     size: {
@@ -25,6 +31,7 @@ for(let i = 1; i <= objectsCount; i++){
     opacity,
     positionTop,
     animation:{
+      delay,
       duration,
       translationStart,
       translationEnd,
--- scripts/home.js
+++ scripts/home.js
@@ -2,16 +2,17 @@ let movingObjectsHTML = '';
 
 movingObjects.forEach((element) => {
   movingObjectsHTML += `
-    <div style="
+    <div class="moving-object" style="
       width: ${element.size.width}px;
       height: ${element.size.height}px;
       background: ${element.color};
       opacity: ${element.opacity}%;
-
-
+      
       position: absolute;
       top: ${element.positionTop}%;
       transform: translateX(100%);
+      transform: translateX(-10000px);
+      transform: translateX(-10vw);
       
       --animation-duration: ${element.animation.duration}s;
       --translation-start: ${element.animation.translationStart}vw;
@@ -21,10 +22,19 @@ movingObjects.forEach((element) => {
       
       
       animation: slide-and-rotate var(--animation-duration) linear infinite;
+      animation-delay: ${element.animation.delay}s;
     "></div>
   `
+
+  const randomOpacity = Math.random();
+  setTimeout(function() {
+    element.style.opacity = 1;
+  },100);
+
 })
 
+
+//opacity: ${element.opacity}%;
 console.log(movingObjectsHTML);
 
 document.querySelector('.js-movingObjects').innerHTML = movingObjectsHTML;
